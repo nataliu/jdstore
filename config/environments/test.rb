ac8b5bda6f5fec6e9b778339c70e10853046f390
“nataliu” <13636424061@163.com>
2017-02-22 18:03:11 +0800

    heroku error

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,12 +29,13 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = {:host => "localhost:3000"} 
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -42,5 +43,3 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
-
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
