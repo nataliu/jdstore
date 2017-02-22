8abbfc3df281254861280914deb42cb1a8f131ef
“nataliu” <13636424061@163.com>
2017-02-22 16:20:03 +0800

    carrierwave update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAIO575NHSO3YGLPBA"],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
-      aws_secret_access_key: ENV["88Dy7TMdtMVMMYKJtOy8ZRvCE9gq2gdnfNHIkk78"],        
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                ENV["ap-northeast-1"]
+      region:                ENV["AWS_REGION"]
 
     }
-    config.fog_directory  = ENV["jdstore01"]
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
 
 
   else
