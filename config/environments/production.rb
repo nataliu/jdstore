7845b7ba98d19fdd5a5d424d52d80648c7052047
“nataliu” <13636424061@163.com>
2017-02-23 16:03:14 +0800

    heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.default_url_options = { host: 'https://aqueous-atoll-62567.herokuapp.com' }  #if it is local then 'localhost:3000'
-
-  Rails.application.routes.default_url_options[:host] = 'domain.com'
+  # config.action_mailer.default_url_options = { host: 'https://aqueous-atoll-62567.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
+  heroku config:set MAILER_URL=https://aqueous-atoll-62567.herokuapp.com
 
 end
